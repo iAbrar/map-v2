eda2012ca17b6ed940146f0a7d75f50aa1323d94
iAbrar <abrar.ksu@gmail.com>
2017-05-31 10:20:18 +0300

    add marker style

--- js/app.js
+++ js/app.js
@@ -213,6 +213,12 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
     largeInfowindow = new google.maps.InfoWindow();
 
+     // Style the markers a bit. This will be our listing marker icon.
+        var defaultIcon = makeMarkerIcon('0091ff');
+        // Create a "highlighted location" marker color for when the user
+        // mouses over the marker.
+        var highlightedIcon = makeMarkerIcon('FFFF24');
+    
     showMarkers(vm.locationsArray());
 
 }
@@ -256,7 +262,16 @@ function showMarkers(locations) {
 
 }
 
-
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+      }
 
 // *******************************
 // *      ERROR Handling         *
