793f9758d966c373fd872c3f30c50ee173afa044
iAbrar <abrar.ksu@gmail.com>
2017-05-31 10:39:26 +0300

    declare icons

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var map;
 var largeInfowindow;
-
+var defaultIcon, highlightedIcon;
 
 var initialLocation = [{
     name: "Nino",
@@ -214,10 +214,10 @@ function initialize() {
     largeInfowindow = new google.maps.InfoWindow();
 
      // Style the markers a bit. This will be our listing marker icon.
-        var defaultIcon = makeMarkerIcon('0091ff');
+       defaultIcon = makeMarkerIcon('0091ff');
         // Create a "highlighted location" marker color for when the user
         // mouses over the marker.
-        var highlightedIcon = makeMarkerIcon('FFFF24');
+        highlightedIcon = makeMarkerIcon('FFFF24');
     
     showMarkers(vm.locationsArray());
 
